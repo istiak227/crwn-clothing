5c69e0b10fff035c6541ec898697d5e9c461edce
unknown <istiak.ahmed227@gmail.com>
2020-08-22 00:16:27 +0600

    added auth into App.js

--- src/App.js
+++ src/App.js
@@ -8,23 +8,43 @@ import ShopPage from './pages/shop/shop.component';
 import Contact from './pages/contact/contact.component';
 import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 
 
 
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/signin' component={SignInSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+      console.log(user);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/signin' component={SignInSignUp} />
+        </Switch>
+      </div>
+    );
+
+
+  }
 }
 
 export default App;
